2cb82f48df20be8784262b8c870fd91a925b09b2
Kyle Chadha <kyle.chadha@buzzfeed.com>
2014-10-13 14:01:52 -0400

    Define filter bar module

---
+++ js/module-filterbar.js
@@ -0,0 +1,24 @@
+CORE.create_module('filters-bar', function(sb) {
+  var filters;
+
+  return {
+
+    init: function() {
+      filters = sb.find('a');
+      sb.addEvent(filters, 'click', this.filterProducts);
+    },
+
+    destroy: function() {
+      sb.removeEvent(filters, 'click', this.filterProducts);
+      filters = null;
+    },
+
+    filterProducts: function(e) {
+      sb.notify({
+        type: 'change-filter',
+        data: e.currentTarget.innerHTML
+      })
+    }
+
+  }
+})
